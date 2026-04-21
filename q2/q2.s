b2e777ab4b79d1e36db589c6b957862009ae698f
shaurya <shaurya21feb@gmail.com>
2026-04-21 16:42:24 +0530

    Fix q2 RISC-V next greater element implementation

--- q2/q2.s
+++ q2/q2.s
@@ -1,3 +1,9 @@
+.section .rodata
+fmt_first:
+    .string "%d"
+fmt_next:
+    .string " %d"
+
 .text
 .globl main
 .type main, @function
@@ -11,132 +17,145 @@ main:
     sd s4, 32(sp)
     sd s5, 24(sp)
     sd s6, 16(sp)
-    
-    # a0 = argc, a1 = argv
-    mv s0, a0
+
     mv s1, a1
-    addi s0, s0, -1
-    
-    bgt s0, x0, .alloc_arrays
-    li a0, 10
-    call putchar
-    li a0, 0
-    j .main_done
-    
-.alloc_arrays:
+    addiw s0, a0, -1
+
+    blez s0, .print_newline_only
+
     slli a0, s0, 2
-    sd a0, 0(sp)
     call malloc
     mv s2, a0
-    beq s2, x0, .main_err
-    
-    ld a0, 0(sp)
+    beqz s2, .main_err
+
+    slli a0, s0, 2
     call malloc
     mv s3, a0
-    beq s3, x0, .main_err
-    
-    ld a0, 0(sp)
+    beqz s3, .free_arr_err
+
+    slli a0, s0, 2
     call malloc
     mv s4, a0
-    beq s4, x0, .main_err
-    
+    beqz s4, .free_res_arr_err
+
+    li s6, 0
 .parse_loop:
-    li s5, 0
-    
-.parse_loop_iter:
-    bge s5, s0, .compute_nge
-    li a0, 1
-    add a0, a0, s5
-    slli a0, a0, 3
-    add a0, a1, a0
-    ld a0, 0(a0)
+    bge s6, s0, .compute_nge
+    addi t0, s6, 1
+    slli t0, t0, 3
+    add t0, s1, t0
+    ld a0, 0(t0)
     call atoi
-    
-    slli a1, s5, 2
-    add a2, s2, a1
-    sw a0, 0(a2)
-    li a0, -1
-    add a2, s3, a1
-    sw a0, 0(a2)
-    
-    addi s5, s5, 1
-    j .parse_loop_iter
-    
+
+    slli t1, s6, 2
+    add t2, s2, t1
+    sw a0, 0(t2)
+    add t2, s3, t1
+    li t3, -1
+    sw t3, 0(t2)
+
+    addi s6, s6, 1
+    j .parse_loop
+
 .compute_nge:
     li s5, -1
-    addi a0, s0, -1
-    
-.nge_loop:
-    bltz a0, .print_result
-    
-.nge_pop:
-    blez s5, .nge_push
-    slli a1, s5, 2
-    add a2, s4, a1
-    lw a1, 0(a2)
-    slli a2, a1, 2
-    add a3, s2, a2
-    lw a1, 0(a3)
-    slli a2, a0, 2
-    add a3, s2, a2
-    lw a2, 0(a3)
-    bgt a1, a2, .nge_push
+    addi s6, s0, -1
+
+.outer_loop:
+    blt s6, x0, .print_result
+
+.pop_loop:
+    blt s5, x0, .after_pop
+    slli t0, s5, 2
+    add t0, s4, t0
+    lw t1, 0(t0)
+
+    slli t2, t1, 2
+    add t2, s2, t2
+    lw t3, 0(t2)
+
+    slli t4, s6, 2
+    add t4, s2, t4
+    lw t5, 0(t4)
+
+    ble t3, t5, .do_pop
+    j .after_pop
+
+.do_pop:
     addi s5, s5, -1
-    j .nge_pop
-    
-.nge_push:
-    slli a1, a0, 2
-    blez s5, .nge_store
-    slli a2, s5, 2
-    add a3, s4, a2
-    lw a2, 0(a3)
-    add a3, s3, a1
-    sw a2, 0(a3)
-    
-.nge_store:
+    j .pop_loop
+
+.after_pop:
+    blt s5, x0, .push_current
+    slli t0, s5, 2
+    add t0, s4, t0
+    lw t1, 0(t0)
+    slli t2, s6, 2
+    add t2, s3, t2
+    sw t1, 0(t2)
+
+.push_current:
     addi s5, s5, 1
-    slli a1, s5, 2
-    add a2, s4, a1
-    sw a0, 0(a2)
-    
-    addi a0, a0, -1
-    j .nge_loop
-    
+    slli t0, s5, 2
+    add t0, s4, t0
+    sw s6, 0(t0)
+
+    addi s6, s6, -1
+    j .outer_loop
+
 .print_result:
-    li s5, 0
-    
+    li s6, 0
+
 .print_loop:
-    bge s5, s0, .print_done
-    slli a0, s5, 2
-    add a1, s3, a0
-    lw a1, 0(a1)
-    beq s5, x0, .print_first
-    li a0, ' '
-    call putchar
-    
+    bge s6, s0, .print_done
+    slli t0, s6, 2
+    add t0, s3, t0
+    lw a1, 0(t0)
+
+    beq s6, x0, .print_first
+    la a0, fmt_next
+    call printf
+    j .print_advance
+
 .print_first:
-    mv a0, a1
-    call print_int
-    addi s5, s5, 1
+    la a0, fmt_first
+    call printf
+
+.print_advance:
+    addi s6, s6, 1
     j .print_loop
-    
+
 .print_done:
     li a0, 10
     call putchar
-    
+
     mv a0, s4
     call free
     mv a0, s3
     call free
     mv a0, s2
     call free
-    
+
     li a0, 0
     j .main_done
-    
+
+.print_newline_only:
+    li a0, 10
+    call putchar
+    li a0, 0
+    j .main_done
+
+.free_res_arr_err:
+    mv a0, s3
+    call free
+
+.free_arr_err:
+    mv a0, s2
+    call free
+
 .main_err:
     li a0, 1
-    
+
 .main_done:
     ld s6, 16(sp)
     ld s5, 24(sp)
@@ -147,20 +166,6 @@ main:
     ld s0, 64(sp)
     ld ra, 72(sp)
     addi sp, sp, 80
-    ret
-
-.globl print_int
-.type print_int, @function
-print_int:
-    addi sp, sp, -16
-    sd ra, 8(sp)
-    
-    mv a1, a0
-    li a0, 10
-    call printf
-    
-    ld ra, 8(sp)
-    addi sp, sp, 16
     ret
 
 .section .note.GNU-stack,"",@progbits
